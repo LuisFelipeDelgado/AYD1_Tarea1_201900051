a8ec4e09c9fd5fc232307166ad1792848d94d603
LuisFelipeDelgado <2998485580101@ingenieria.usac.edu.gt>
2022-06-07 22:55:35 -0600

    Prueba

--- main3000.py
+++ main3000.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import request
 from flask_cors import CORS
 
@@ -20,14 +20,10 @@ def resta():
 
     return str(result)
 
-@app.route('/multiplicacion', methods = ['POST'])
-def multiplicacion():
-    numero1 = int(request.json['numero1'])
-    numero2 = int(request.json['numero2'])
+@app.route('/alreves/palabra:<string:word>', methods=["GET"])
+def multiplicacion(word):
 
-    result = numero1*numero2
-
-    return str(result)
+    return str(word[::-1])
 
 if (__name__ == '__main__'):
     app.run(port = 3000, debug = True)
--- main4000.py
+++ main4000.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def initServer(): 
     return {
-        'response':'Servidor corriendo'
+        'message':"Servidor corriendo"
     }
 
-@app.route('/info', methods=["GET"])
-def info():
-    return "Nombre: Luis Felipe Delgado Benitez \n Carnet: 201900051"
+@app.route('/alreves/palabra:<string:word>', methods=["GET"])
+def alreves(word):
 
+    return {'message' :word[::-1]}
+    
 if (__name__ == '__main__'):
     app.run(port = 4000, debug = True)
